be522a1af862741d156819e426cef0ba26c637ef
joepickrell <joepickrell@gmail.com>
2014-06-30 10:52:21 -0400

    plotting

--- sims/scripts/plot_baseline_0.1.R
+++ sims/scripts/plot_baseline_0.1.R
@@ -9,7 +9,7 @@ getrange = function(f){
 	return( c(mean(d[6:95]), d[6], d[95]))
 }
 
-fs = c("all_0.1_0.01.fs", "all_0.2_0.01.fs", "all_0.3_0.01.fs", "all_0.4_0.01.fs")
+fs = c("all_0.1_0.1.fs", "all_0.2_0.1.fs", "all_0.3_0.1.fs", "all_0.4_0.1.fs")
 ffs = c(0.1, 0.2, 0.3, 0.4)
 toplot = data.frame(matrix(nrow = 4, ncol = 5))
 
@@ -33,7 +33,7 @@ for (i in 1:length(fs)){
 }
 print(toplot2)
 
-pdf("plot_baseline_0.01.pdf")
+pdf("plot_baseline_0.1.pdf")
 plot(toplot[,1], toplot[,3], pch = 20, xlim = c(0, 0.5), ylim = c(0, 0.6), xlab = "Simulated prop. of causal variants in annotation", ylab= "Estimated prop. of causal variants in annotation")
 for (i in 1:nrow(toplot)){
 	lines( c(toplot[i,1], toplot[i,1]), c(toplot[i,4], toplot[i,5]))
@@ -44,8 +44,8 @@ points(toplot2[,1]+0.01, toplot2[,3], pch = 20, xlim = c(0, 0.5), ylim = c(0, 0.
 for (i in 1:nrow(toplot2)){
         lines( c(toplot2[i,1]+0.01, toplot2[i,1]+0.01), c(toplot2[i,4], toplot2[i,5]), col = "grey")
 }
-lines(c(-10, 10), c(0.01, 0.01), col = "grey", lty = 2)
-text(0.5, 0.01, lab = "Prop. of non-causal SNPs in annotation", adj = c(1, 1), col = "grey", cex = 0.6)
+lines(c(-10, 10), c(0.1, 0.1), col = "grey", lty = 2)
+text(0.5, 0.09, lab = "Prop. of non-causal SNPs in annotation", adj = c(1, 1), col = "grey", cex = 0.6)
 legend("topleft", legend = c("annotation not in model", "annotation in model"), col = c("grey", "black"), bty = "n", pch = c(20, 20))
 
 dev.off()
